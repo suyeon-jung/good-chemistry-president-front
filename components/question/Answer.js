d37ae813b49687737084a5933eaa74a1d0db17e9
suyeon-jung <wjdjsy210@gmail.com>
2022-02-20 13:44:04 +0900

    [style] : favicon 적용

---
+++ components/question/Answer.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const Answer = () => {
+  return (
+    <Wrapper>
+      <h1>답변</h1>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  border: 1px solid black;
+`;
+export default Answer;
---
+++ pages/test.js
@@ -0,0 +1,14 @@
+import Header from "../components/question/Header";
+import Content from "../components/question/Content";
+import Answer from "../components/question/Answer";
+
+function Test() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Answer />
+    </div>
+  );
+}
+export default Test;
